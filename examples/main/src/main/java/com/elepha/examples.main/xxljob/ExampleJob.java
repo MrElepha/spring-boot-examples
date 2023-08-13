bbb4c58dace79aeb187841b23c46200edac95b48
Mr.elepha <xiang.daicong@gmail.com>
2023-08-13 17:01:17 +0800

    feat: 新增 Redis 配置 & lua 脚本测试

--- examples/main/src/main/java/com/elepha/examples.main/xxljob/ExampleJob.java
+++ examples/main/src/main/java/com/elepha/examples.main/xxljob/ExampleJob.java
@@ -5,8 +5,11 @@ import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -14,7 +17,9 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @Slf4j
@@ -23,6 +28,8 @@ public class ExampleJob {
 
     @Qualifier("asyncTaskExecutor")
     private final Executor threadPool;
+
+    private final StringRedisTemplate stringRedisTemplate;
     private final HttpClient httpClient = HttpClient.newHttpClient();
 
     @XxlJob("hello")
@@ -50,6 +57,30 @@ public class ExampleJob {
                     }
             );
         }
+
+        return ReturnT.SUCCESS;
+    }
+
+    @XxlJob("luaTest")
+    public ReturnT<String> luaTest() {
+        // 先获取指定key的值，然后和传入的arg比较是否相等，相等值删除key，否则直接返回0
+        String luaText = """
+                if redis.call("get",KEYS[1]) == ARGV[1] then
+                    return redis.call("del",KEYS[1])
+                else
+                    return 0
+                end
+                """;
+        String key = "luaTest";
+        String val = RandomStringUtils.randomAlphanumeric(5);
+        if (!Boolean.TRUE.equals(stringRedisTemplate.opsForValue().setIfAbsent(key, val, 1, TimeUnit.MINUTES))) {
+            XxlJobHelper.log("exist");
+        }
+        // 执行 lua 脚本
+        DefaultRedisScript<Long> redisScript = new DefaultRedisScript<>(luaText, Long.class);
+        // 参数一：redisScript，参数二：key列表，参数三：arg（可多个）
+        Long result = stringRedisTemplate.execute(redisScript, List.of(key), val);
+        XxlJobHelper.log("res is {}", result);
 
         return ReturnT.SUCCESS;
     }
